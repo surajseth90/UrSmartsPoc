bc184f2f77c2823a04ccf6f92ea6a65a6b2eb833
surajseth90 <surajseth90@gmail.com>
2025-08-03 18:46:20 +0530

    Updated: Config URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  basePath: "http://64.23.249.156:8080",
+  basePath: "https://64.23.249.156:8443",
   headers: {
     Authorization: "Basic YWRtaW5Ac3lzLmNvbTphZG1pbjEyMw==",
     "Content-Type": "application/json",
